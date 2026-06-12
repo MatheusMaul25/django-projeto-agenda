5f1d7f1d044bd87dcc8403b54326f097f8753ab3
Matheus Maul <matheusmaul64@gmail.com>
2026-06-11 21:47:37 -0300

    Adicionado static root, media url e media root

--- contact/admin.py
+++ contact/admin.py
@@ -3,4 +3,12 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id', 'first_name', 'last_name', 'phone', # Campos que aparecerão
+    ordering = '-id', # Campo que a lista será automaticamente ordenada
+    # list_filter = 'created_date', # Adiciona um filtro a lista
+    search_fields = 'id', 'first_name', 'last_name', 'phone', # Campos que você pode pesquisar
+    list_per_pages = 10 # Itens por página
+    list_max_show_all = 50 # Máximo de itens por página
+    list_editable = 'phone', # Itens editáveis da lista
+    list_display_links = 'id', 'first_name' # Itens clicaveis da lista
+    
