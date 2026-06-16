6fd35098370b50ec0db0e930fd3f45caade5afd8
Matheus Maul <matheusmaul64@gmail.com>
2026-06-16 20:21:31 -0300

    Adicionado owner em Contact

--- contact/models.py
+++ contact/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-        
+
     name = models.CharField(max_length=50)
 
     def __str__(self) -> str:
@@ -27,6 +28,12 @@ class Contact(models.Model):
         blank = True,
         null = True,
         )
+    owner = models.ForeignKey(
+        User,
+        on_delete= models.SET_NULL,
+        blank = True,
+        null = True,
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
