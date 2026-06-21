3bff255c75bb71b4a39d3dc374ad34e785bd9c2c
Matheus Maul <matheusmaul64@gmail.com>
2026-06-21 14:50:22 -0300

    Adicionado contact.html, adicionado url de id unico

--- contact/admin.py
+++ contact/admin.py
@@ -3,13 +3,13 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id', 'first_name', 'last_name', 'phone', 'category', # Campos que aparecerão
+    list_display = 'id', 'first_name', 'last_name', 'phone', 'category', 'show', # Campos que aparecerão
     ordering = '-id', # Campo que a lista será automaticamente ordenada
     # list_filter = 'created_date', # Adiciona um filtro a lista
     search_fields = 'id', 'first_name', 'last_name', 'phone', 'category__name', # Campos que você pode pesquisar
     list_per_pages = 10 # Itens por página
     list_max_show_all = 50 # Máximo de itens por página
-    list_editable = 'phone', # Itens editáveis da lista
+    list_editable = 'phone', 'show', # Itens editáveis da lista
     list_display_links = 'id', 'first_name' # Itens clicaveis da lista
 
 @admin.register(models.Category)
--- contact/urls.py
+++ contact/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from . import views
-from django.conf.urls.static import static
-from django.conf import settings
+from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('<int:contact_id>/', views.contact, name = 'contact'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
