08fc31924cabe146edad778efe377ae5e370aeb9
Matheus Maul <matheusmaul64@gmail.com>
2026-06-21 17:02:37 -0300

    Adicionado tratamento de erro na view contact

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 # Create your views here.
@@ -15,8 +15,13 @@ def index(request):
         )
 
 def contact(request, contact_id):
-    
-    single_contact = Contact.objects.get(pk=contact_id)
+
+    single_contact = get_object_or_404(Contact, pk=contact_id, show = True)
+
+    # single_contact = get_object_or_404(
+    #     Contact.objects.filter(show = True), 
+    #     pk=contact_id
+    # )
 
     context = {
         'contact': single_contact,
